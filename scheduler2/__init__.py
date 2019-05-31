69bbe3956a406516a29a6a6da4e93d1af291087c
Michael H <michael@michaelhall.tech>
2019-05-31 19:16:44 -0400

    wip

--- scheduler2/__init__.py
+++ scheduler2/__init__.py
@@ -3,10 +3,14 @@ from redbot import version_info, VersionInfo
 from .core import Scheduler
 
 
-def setup(bot):
+async def setup(bot):
     if version_info < VersionInfo.from_str("3.1.2"):
         raise errors.CogLoadError("You need at least version `3.1.2`")
     if bot.user.id != 275047522026913793:
-        raise errors.CogLoadError("You really shouldn't try and play with undocumented things yet.")
-    
-    bot.add_cog(Scheduler(bot))
+        raise errors.CogLoadError(
+            "You really shouldn't try and play with undocumented things yet."
+        )
+
+    cog = Scheduler(bot)
+    await cog.initalize()
+    bot.add_cog(cog)
